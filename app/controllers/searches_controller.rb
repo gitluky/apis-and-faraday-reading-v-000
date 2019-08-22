b8ef788c26e5994d73378f61767b43f4c977fe11
Jonathan Luk <jluk87@gmail.com>
2019-08-22 21:39:47 +0000

    Automatically backed up by Learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -22,9 +22,9 @@ class SearchesController < ApplicationController
     rescue Faraday::ConnectionFailed
       @error = "There was a timeout. Please try again."
     end
-    
+
     render 'search'
-    
+
   end
 
 end
